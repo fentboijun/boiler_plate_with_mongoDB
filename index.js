28caa00860d1d575966892eec151461054173e3a
Ha hyeon jun <81820723+fentboijun@users.noreply.github.com>
2023-10-30 01:03:55 +0900

    comitted

--- index.js
+++ index.js
@@ -1,18 +1,75 @@
-import  express  from 'express';
-import mongoose  from 'mongoose';
-import mongoSetting from './mongoSetting.js';
+import express from "express";
+import mongoose from "mongoose";
+import mongoSetting from "./mongoSetting.js";
+import User from "./models/User.js";
+import bodyParser from "body-parser";
 const app = express();
 const port = 8080;
 // 몽고 DB연결
-mongoose.connect(`mongodb+srv://${mongoSetting.userName}:${mongoSetting.password}@${mongoSetting.dbName}.3vjerv8.mongodb.net/?retryWrites=true&w=majority`,{
-    useNewUrlParser:true,useUnifiedTopology:true
-}).then(() => console.log('MongoDb connected'))
-.catch(err => console.error(err))
+mongoose
+  .connect(
+    `mongodb+srv://${mongoSetting.userName}:${mongoSetting.password}@${mongoSetting.dbName}.3vjerv8.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDb connected"))
+  .catch((err) => console.error(err));
+// 바디파서 사용
+// app use를 이용하여 등록함
+// application/x-www-form-urlencoded 분석해서 가져올수있게
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json 분석해서 가져올수있게
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+// user Create 작업
+app.post("/register", async (req, res) => {
+  //회원가입시 필요 정보를 client에서 가져오면
+  //데이터베이스에 삽입한다
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  //body parser를 통해 body에 담긴 정보를 가져온다
+  const user = new User(req.body);
 
+  //mongoDB 메서드, user모델에 저장
+  const result = await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      res.json({ success: false, err });
+    });
+});
+app.post("/login", async (req, res) => {
+  // mongo에서 요청된 이메일을 데이터 베이스에서 있는지 찾기
+  // 비밀번호 까지 확인
+  // 맞다면 토큰 생성 후 집어넣기
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.json({
+          loginSuccess: false,
+          message: "제공된 이메일에 해당하는 유저가 없습니다.",
+        });
+      }
+      user.checkPw(req.body.password, (err, isMatch) => {
+        if (!isMatch)
+          return res.json({
+            loginSuccess: false,
+            message: "비밀번호가 일치하지 않습니다.",
+          });
+        user.createToken((err, user) => {});
+      });
+    })
+    .catch((err) => {
+      res.json({ loginSuccess: false, err });
+    });
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- models/User.js
+++ models/User.js
@@ -1,36 +1,67 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt";
+const saltRounds = 10;
+// salt암호화 saltRounds = salt.length
 const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        maxlength:50
-    },
-    email:{
-        type:String,
-        trim:true,
-        unique:1
-        // trim 은 공백제거 여부
-        // fent boi jun@gmail.com -> 
-    },
-    password:{
-        type:String,
-        maxlength:20
-    },
-    role:{
-        type:Number,
-        default:0,
-        // 0 유저
-        // 2 관리자
-    },
-    image:String,
-    token:{
-        type:String
-    },
-    tokenExp:{
-        type:Number
-    },
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+    // trim 은 공백제거 여부
+    // fent boi jun@gmail.com ->
+  },
+  password: {
+    type: String,
+    maxlength: 100,
+  },
+  role: {
+    type: Number,
+    default: 0,
+    // 0 유저
+    // 2 관리자
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
 });
 
-const User = mongoose.model('User',userSchema);
+// save 하기 전에 할 작업들
+userSchema.pre("save", function(next){
+  // 위 스키마 지정
+  const user = this;
+  console.log(user);
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      // salt 생성에 실패하면 에러 리턴
+      if (err) return next(err);
+      // 암호화
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        // 암호화 성공되면 위 스키마의 password 값을 hash값으로 바꿈
+        user.password = hash;
+        next();
+      });
+    });
+  }else{
+    next();
+  }
+});
+// 그 이후 저장
+userSchema.methods.checkPw = function(plainPassword,cb){
+
+    bcrypt.compare(plainPassword,this.password,function (err,isMatch){
+        if(err) return cb(err);
+        cb(null,isMatch);
+    })
+}
+const User = mongoose.model("User", userSchema);
 
 export default User;
